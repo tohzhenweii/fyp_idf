347953ec77a75001304c2b2ccd3f102e6fb8f515
Khor Yong Kai <yk2796@gmail.com>
2020-09-28 17:05:30 +0800

    UI Improvement v1.0 + Debug v1.0
    Improved MainActivity, ActivityList UI. Added new icons. Deleted unneccessary files.

--- Android-SDK-master-Navigine/NavigineDemo/app/src/main/java/com/navigine/naviginedemo/ApplicationTestCase.kt
+++
@@ -1,7 +0,0 @@
-package com.navigine.naviginedemo
-
-import android.app.Application
-import ContextMenu.ContextMenuInfo
-import AdapterView.AdapterContextMenuInfo
-
-internal class ApplicationTestCase<T>(applicationClass: Class<Application?>?)
