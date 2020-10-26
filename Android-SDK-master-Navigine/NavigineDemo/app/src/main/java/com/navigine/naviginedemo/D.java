b00c616fe08a44adac2b7dc9e6fe71f6a79880da
Yong Kai <yk2796@gmail.com>
2020-10-26 16:10:47 +0800

    D update
    Data update

--- Android-SDK-master-Navigine/NavigineDemo/app/src/main/java/com/navigine/naviginedemo/D.java
+++ Android-SDK-master-Navigine/NavigineDemo/app/src/main/java/com/navigine/naviginedemo/D.java
@@ -2,6 +2,6 @@ package com.navigine.naviginedemo;
 public final class D {
   public static final String  SERVER_URL    = "https://api.navigine.com";
   public static final String  USER_HASH     = "D11D-8F95-2F76-A435";
-  public static final String  LOCATION_NAME = "Blk L - Level 3";
+  public static final String  LOCATION_NAME = "Blk L";
   public static final boolean WRITE_LOGS    = false;
 }
